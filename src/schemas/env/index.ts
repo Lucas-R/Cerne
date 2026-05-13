64ccd92feb85af57a4fa9ce1f13783a645e82a2f
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2026-05-13 17:44:54 -0300

    fix: corrige local  do  arquivo  .env e .env.example

--- src/schemas/env/index.ts
+++ src/schemas/env/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const EnvSchema = z.object({
-    DATABASE_URL: z.url("Database URL is required")
+    DATABASE_URL: z.string("Database URL is required")
 });
 
 export type EnvProps = z.infer<typeof EnvSchema>;
